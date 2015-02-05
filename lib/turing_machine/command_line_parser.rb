43781aa68440fe78207355c350c7beae4ed56a46
lkdjiin <xavier.nayrac@gmail.com>
2015-02-05 14:52:08 +0100

    Display help when no args are providen

--- lib/turing_machine/command_line_parser.rb
+++ lib/turing_machine/command_line_parser.rb
@@ -5,15 +5,16 @@ module TuringMachine
 
   class CommandLineParser
 
-    def self.parse(args)
-      options = OpenStruct.new
-      options.tape = '0'
+    def initialize(args)
+      @args = args
+      @options = OpenStruct.new
+      @options.tape = '0'
 
-      opt_parser = OptionParser.new do |opts|
+      @opt_parser = OptionParser.new do |opts|
         opts.banner = "Usage: turing_machine instruction_set [options]"
 
         opts.on("-t", "--tape DATA", "Initialize the tape with DATA") do |data|
-          options.tape = data
+          @options.tape = data
         end
 
         opts.on_tail("-h", "--help", "Show this message") do
@@ -26,9 +27,15 @@ module TuringMachine
           exit
         end
       end
+    end
 
-      opt_parser.parse!(args)
-      options
+    def parse
+      @opt_parser.parse!(@args)
+      @options
+    end
+
+    def help
+      @opt_parser.help
     end
 
   end
--- spec/command_line_parser_spec.rb
+++ spec/command_line_parser_spec.rb
@@ -5,17 +5,20 @@ include TuringMachine
 describe CommandLineParser do
 
   it 'parses -t' do
-    options = CommandLineParser.parse %w( -t 11011 )
+    cli_parser = CommandLineParser.new %w( -t 11011 )
+    options = cli_parser.parse
     expect(options.tape).to eq '11011'
   end
 
   it 'parses --tape' do
-    options = CommandLineParser.parse %w( --tape 101 )
+    cli_parser = CommandLineParser.new %w( --tape 101 )
+    options = cli_parser.parse
     expect(options.tape).to eq '101'
   end
 
   specify 'tape default value' do
-    options = CommandLineParser.parse []
+    cli_parser = CommandLineParser.new []
+    options = cli_parser.parse
     expect(options.tape).to eq '0'
   end
 
